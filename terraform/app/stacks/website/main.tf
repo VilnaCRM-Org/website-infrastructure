ba84858cc5fbf2e52be51ea3b305383e8dc7e97b
Sokolov Valentyn <sokolval555@gmail.com>
2024-01-10 14:09:44 +0200

    fix(#2): change from env to repo variable

--- terraform/app/stacks/website/main.tf
+++ terraform/app/stacks/website/main.tf
@@ -1,5 +1,5 @@
 module "static-website-s3-cloudfront-acm" {
-  source      = "git::github.com/joshuamkite/terraform-aws-static-website-s3-cloudfront-acm/tree/2.2.0/examples/simple-complete-website-with-sample-content?ref=dc27b0842a453458b7294352b3c8751198a33110"
+  source      = "git::https://github.com/joshuamkite/terraform-aws-static-website-s3-cloudfront-acm/tree/2.2.0/examples/simple-complete-website-with-sample-content?ref=dc27b0842a453458b7294352b3c8751198a33110"
   domain_name = var.site_domain
   region      = var.aws_region
 }
