521a6742b2b68de88286ca1d3f5a66da5e79b48a
Sokolov Valentyn <sokolval555@gmail.com>
2024-01-10 15:21:43 +0200

    fix(#2): run terraform fmt and change version

--- terraform/config/terraform/terraform.tf
+++ terraform/config/terraform/terraform.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 1.3"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.25.0"
+      source                = "hashicorp/aws"
+      version               = ">= 5.25.0"
       configuration_aliases = [aws.us-east-1]
     }
     random = {
