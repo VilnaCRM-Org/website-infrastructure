4fe8a11094015d908a2e4e90335b0978d2c92e1f
YapieRT <sokolov.valentyn.work@gmail.com>
2024-01-15 19:51:35 +0200

    fix(#2): reorganize kms

--- terraform/app/modules/aws/kms/data.tf
+++ terraform/app/modules/aws/kms/data.tf
@@ -1,2 +1,74 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
+
+data "aws_iam_policy_document" "kms_key_policy_doc" {
+  statement {
+    sid     = "Enable IAM User Permissions"
+    effect  = "Allow"
+    actions = ["kms:*"]
+    #checkov:skip=CKV_AWS_356:Without this statement, KMS key cannot be managed by root
+    resources = ["*"]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${local.account_id}:root"]
+    }
+  }
+
+  statement {
+    sid       = "Allow access for Key Administrators"
+    effect    = "Allow"
+    actions   = ["kms:*"]
+    resources = ["*"]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        var.codepipeline_role_arn
+      ]
+    }
+  }
+
+  statement {
+    sid    = "Allow use of the key"
+    effect = "Allow"
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey"
+    ]
+    resources = ["*"]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        var.codepipeline_role_arn
+      ]
+    }
+  }
+
+  statement {
+    sid    = "Allow attachment of persistent resources"
+    effect = "Allow"
+    actions = [
+      "kms:CreateGrant",
+      "kms:ListGrants",
+      "kms:RevokeGrant"
+    ]
+    resources = ["*"]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        var.codepipeline_role_arn
+      ]
+    }
+
+    condition {
+      test     = "Bool"
+      variable = "kms:GrantIsForAWSResource"
+      values   = ["true"]
+    }
+  }
+}
