c2f65a39199c975257da5426a6247704b44933c1
YapieRT <sokolov.valentyn.work@gmail.com>
2024-01-15 15:42:17 +0200

    feat(#2): add base s3 terraform code

---
+++ terraform/app/modules/aws/s3/data.tf
@@ -0,0 +1,24 @@
+data "aws_iam_policy_document" "bucket_policy_doc_codepipeline_bucket" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = [var.codepipeline_role_arn]
+    }
+
+    actions = [
+      "s3:Get*",
+      "s3:List*",
+      "s3:ReplicateObject",
+      "s3:PutObject",
+      "s3:RestoreObject",
+      "s3:PutObjectVersionTagging",
+      "s3:PutObjectTagging",
+      "s3:PutObjectAcl"
+    ]
+
+    resources = [
+      aws_s3_bucket.codepipeline_bucket.arn,
+      "${aws_s3_bucket.codepipeline_bucket.arn}/*",
+    ]
+  }
+}
--- terraform/app/modules/aws/s3/main.tf
+++ terraform/app/modules/aws/s3/main.tf
@@ -1,1 +1,48 @@
-# This is where you put your resource declaration
+#Artifact Bucket
+resource "aws_s3_bucket" "codepipeline_bucket" {
+  bucket_prefix = regex("[a-z0-9.-]+", lower(var.project_name))
+  tags          = var.tags
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "codepipeline_bucket_access" {
+  bucket                  = aws_s3_bucket.codepipeline_bucket.id
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+  block_public_acls       = true
+  block_public_policy     = true
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy_codepipeline_bucket" {
+  bucket = aws_s3_bucket.codepipeline_bucket.id
+  policy = data.aws_iam_policy_document.bucket_policy_doc_codepipeline_bucket.json
+}
+
+resource "aws_s3_bucket_acl" "codepipeline_bucket_acl" {
+  bucket = aws_s3_bucket.codepipeline_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "codepipeline_bucket_versioning" {
+  bucket = aws_s3_bucket.codepipeline_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "codepipeline_bucket_encryption" {
+  bucket = aws_s3_bucket.codepipeline_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = var.kms_key_arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_logging" "codepipeline_bucket_logging" {
+  bucket        = aws_s3_bucket.codepipeline_bucket.id
+  target_bucket = aws_s3_bucket.codepipeline_bucket.id
+  target_prefix = "log/"
+}
