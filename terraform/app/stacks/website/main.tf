f9688b1018eaf14a8c2e744c8718c1e399a41ff7
YapieRT <sokolov.valentyn.work@gmail.com>
2024-01-15 11:06:38 +0200

    fix(#2): update base.tfvars and change module inputs

--- terraform/app/stacks/website/main.tf
+++ terraform/app/stacks/website/main.tf
@@ -1,7 +1,6 @@
 module "static-website-s3-cloudfront-acm" {
   source      = "git::https://github.com/joshuamkite/terraform-aws-static-website-s3-cloudfront-acm.git?ref=8103476a4ed0a960a6bd1ebe1f012d66525ce78a"
   domain_name = var.site_domain
-  region      = var.aws_region
   providers = {
     aws.us-east-1 = aws.us-east-1
     aws           = aws
