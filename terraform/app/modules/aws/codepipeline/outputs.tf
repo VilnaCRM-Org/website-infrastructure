f023f4b40510e5c00ff1fee3718cf33a2a1492b4
YapieRT <sokolov.valentyn.work@gmail.com>
2024-01-16 16:54:57 +0200

    feat(#2): add general codepipeline

--- terraform/app/modules/aws/codepipeline/outputs.tf
+++ terraform/app/modules/aws/codepipeline/outputs.tf
@@ -1,1 +1,15 @@
-# This is where you put your outputs declaration
+output "id" {
+  value       = aws_codepipeline.terraform_pipeline.id
+  description = "The id of the CodePipeline"
+}
+
+output "name" {
+  value       = aws_codepipeline.terraform_pipeline.name
+  description = "The name of the CodePipeline"
+}
+
+output "arn" {
+  value       = aws_codepipeline.terraform_pipeline.arn
+  description = "The arn of the CodePipeline"
+}
+
