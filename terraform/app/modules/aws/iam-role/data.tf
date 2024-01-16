0145291610ab999cda9b6d6a11164b48b76df123
YapieRT <sokolov.valentyn.work@gmail.com>
2024-01-16 10:25:34 +0200

    fix(#2): separate into different files iam policy

--- terraform/app/modules/aws/iam-role/data.tf
+++ terraform/app/modules/aws/iam-role/data.tf
@@ -7,3 +7,111 @@ data "aws_iam_role" "existing_codepipeline_role" {
   count = var.create_new_role ? 0 : 1
   name  = var.codepipeline_iam_role_name
 }
+
+data "aws_iam_policy_document" "codepipeline_role_document" {
+  source_json = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Effect": "Allow"
+    },
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+data "aws_iam_policy_document" "codepipeline_policy_document" {
+  source_json = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect":"Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:GetObjectVersion",
+        "s3:PutObjectAcl",
+        "s3:PutObject"
+      ],
+      "Resource": "${var.s3_bucket_arn}/*"
+    },
+    {
+      "Effect":"Allow",
+      "Action": [
+        "s3:GetBucketVersioning"
+      ],
+      "Resource": "${var.s3_bucket_arn}"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+         "kms:DescribeKey",
+         "kms:GenerateDataKey*",
+         "kms:Encrypt",
+         "kms:ReEncrypt*",
+         "kms:Decrypt"
+      ],
+      "Resource": "${var.kms_key_arn}"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+         "codecommit:GitPull",
+         "codecommit:GitPush",
+         "codecommit:GetBranch",
+         "codecommit:CreateCommit",
+         "codecommit:ListRepositories",
+         "codecommit:BatchGetCommits",
+         "codecommit:BatchGetRepositories",
+         "codecommit:GetCommit",
+         "codecommit:GetRepository",
+         "codecommit:GetUploadArchiveStatus",
+         "codecommit:ListBranches",
+         "codecommit:UploadArchive"
+      ],
+      "Resource": "arn:aws:codecommit:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:${var.source_repository_name}"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codebuild:BatchGetBuilds",
+        "codebuild:StartBuild",
+        "codebuild:BatchGetProjects"
+      ],
+      "Resource": "arn:aws:codebuild:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:project/${var.project_name}*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codebuild:CreateReportGroup",
+        "codebuild:CreateReport",
+        "codebuild:UpdateReport",
+        "codebuild:BatchPutTestCases"
+      ],
+      "Resource": "arn:aws:codebuild:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:report-group/${var.project_name}*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:${data.aws_partition.current.partition}:logs:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:log-group:*"
+    }
+  ]
+}
+EOF
+}
