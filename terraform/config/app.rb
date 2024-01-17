0036221879fb88bb4d191a7be5feead90904cd31
YapieRT <sokolov.valentyn.work@gmail.com>
2024-01-17 18:47:30 +0200

    fix(#2): update terraspace config

--- terraform/config/app.rb
+++ terraform/config/app.rb
@@ -1,7 +1,7 @@
 # Docs: https://terraspace.cloud/docs/config/reference/
 Terraspace.configure do |config|
   # config.logger.level = :info
-
+  config.build.copy_modules = true
   # To enable Terraspace Cloud set config.cloud.org
   # config.cloud.org = "ORG"          # required: replace with your org. only letters, numbers, underscore and dashes allowed
   # config.cloud.project = "main"     # optional. main is the default project name. only letters, numbers, underscore and dashes allowed
