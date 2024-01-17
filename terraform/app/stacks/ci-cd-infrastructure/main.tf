478b606b277374f76897f1b5b4216803c220bfec
YapieRT <sokolov.valentyn.work@gmail.com>
2024-01-17 18:46:12 +0200

    fix(#2): reorder code

--- terraform/app/stacks/ci-cd-infrastructure/main.tf
+++ terraform/app/stacks/ci-cd-infrastructure/main.tf
@@ -1,6 +1,7 @@
 module "s3_artifacts_bucket" {
-  source                = "../../modules/aws/s3"
-  project_name          = var.project_name
+  source       = "../../modules/aws/s3"
+  project_name = var.project_name
+
   kms_key_arn           = module.codepipeline_kms.arn
   codepipeline_role_arn = module.codepipeline_iam_role.role_arn
 
@@ -15,13 +16,14 @@ module "codepipeline_kms" {
 }
 
 module "codepipeline_iam_role" {
-  source                     = "../../modules/aws/iam-role"
+  source = "../../modules/aws/iam-role"
+
   project_name               = var.project_name
   create_new_role            = var.create_new_role
   codepipeline_iam_role_name = var.create_new_role == true ? "${var.project_name}-codepipeline-role" : var.codepipeline_iam_role_name
 
-  kms_key_arn                = module.codepipeline_kms.arn
-  s3_bucket_arn              = module.s3_artifacts_bucket.arn
+  kms_key_arn   = module.codepipeline_kms.arn
+  s3_bucket_arn = module.s3_artifacts_bucket.arn
 
   tags = var.tags
 }
@@ -30,7 +32,7 @@ module "codepipeline_terraform" {
   source = "../../modules/aws/codepipeline"
 
   project_name       = var.project_name
-  source_repo_owner = var.source_repo_owner
+  source_repo_owner  = var.source_repo_owner
   source_repo_name   = var.source_repo_name
   source_repo_branch = var.source_repo_branch
   github_oauthtoken  = var.github_oauthtoken
--- terraform/app/stacks/ci-cd-infrastructure/tfvars/base.tfvars
+++ terraform/app/stacks/ci-cd-infrastructure/tfvars/base.tfvars
@@ -1,10 +1,10 @@
-project_name     = "vilnacrm-project"
-environment      = "production"
-source_repo_owner = "VilnaCRM-Org"
-source_repo_name = "website-infrastructure"
+project_name       = "vilnacrm-project"
+environment        = "production"
+source_repo_owner  = "VilnaCRM-Org"
+source_repo_name   = "website-infrastructure"
 source_repo_branch = "main"
-github_oauthtoken = ""
+github_oauthtoken  = ""
 tags = {
   Project     = "vilnacrm-project"
-  environment = "production"
+  Environment = "production"
 }
