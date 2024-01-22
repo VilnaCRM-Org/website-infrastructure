5359d31ced9bf365c4d01aed12f856acaac1a871
YapieRT <sokolov.valentyn.work@gmail.com>
2024-01-22 17:27:16 +0200

    fix(#2): remove commented code

--- terraform/app/modules/aws/kms/data.tf
+++ terraform/app/modules/aws/kms/data.tf
@@ -12,11 +12,6 @@ data "aws_iam_policy_document" "kms_key_policy_doc" {
       type        = "AWS"
       identifiers = ["arn:aws:iam::${local.account_id}:root"]
     }
-    # condition {
-    #   test     = "StringEquals"
-    #   variable = "aws:PrincipalType"
-    #   values   = [ "${var.kms_condition_account_value}" ]
-    # }
   }
 
   statement {
