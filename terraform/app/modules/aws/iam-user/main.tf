1e6f06c1290d86f13093ec13740c6aca59de46ec
YapieRT <sokolov.valentyn.work@gmail.com>
2024-01-26 18:19:47 +0200

    fix(#2): fix iam-user

--- terraform/app/modules/aws/iam-user/main.tf
+++ terraform/app/modules/aws/iam-user/main.tf
@@ -1,11 +1,12 @@
 resource "aws_iam_user" "codepipeline_user" {
   name = "${var.project_name}-codepipeline-user"
-
+  #checkov:skip=CKV_AWS_273: Necessary for triggering pipeline
   tags = var.tags
 }
 
 resource "aws_iam_user_policy" "codepipeline_execute_policy" {
-  name   = "test"
+  name   = "${var.project_name}-exec-policy"
+  #checkov:skip=CKV_AWS_40: Necessary for triggering pipeline
   user   = aws_iam_user.codepipeline_user.name
   policy = data.aws_iam_policy_document.codepipeline_execute_policy_doc.json
 }
