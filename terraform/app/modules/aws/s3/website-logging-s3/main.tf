f9b9abad740822c5c6b7a6baddf73755b6b33e9c
YapieRT <sokolov.valentyn.work@gmail.com>
2024-03-04 22:27:03 +0200

    fix(#2): change to logging bucket

--- terraform/app/modules/aws/s3/website-logging-s3/main.tf
+++ terraform/app/modules/aws/s3/website-logging-s3/main.tf
@@ -1,7 +1,9 @@
 resource "aws_s3_bucket" "logging_bucket" {
   bucket_prefix = "${var.project_name}-logging-bucket"
+  #checkov:skip=CKV_AWS_18:The access logging of logging bucket is not needed 
   #checkov:skip=CKV2_AWS_61:The lifecycle configuration of logging bucket is not needed 
-  #checkov:skip=CKV_AWS_145: The KMS encryptiono f logging bucket is not needed 
+  #checkov:skip=CKV2_AWS_62: The event notifications of logging bucket is not needed 
+  #checkov:skip=CKV_AWS_145: The KMS encryption of logging bucket is not needed 
   #checkov:skip=CKV_AWS_144: Replication of logging bucket is not needed 
   tags = var.tags
 }
@@ -9,4 +11,19 @@ resource "aws_s3_bucket" "logging_bucket" {
 resource "aws_s3_bucket_policy" "logging_bucket_policy" {
   bucket = aws_s3_bucket.logging_bucket.id
   policy = data.aws_iam_policy_document.bucket_policy_doc.json
+}
+
+resource "aws_s3_bucket_public_access_block" "logging_bucket_pab" {
+  bucket                  = aws_s3_bucket.logging_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "logging_bucket_versioning" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
