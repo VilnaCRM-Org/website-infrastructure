9735ec8dc7a6eba24b90d9a48c3378fc2e2ec086
YapieRT <sokolov.valentyn.work@gmail.com>
2024-03-18 20:35:48 +0200

    fix(#2): fix new stage

--- terraform/app/modules/aws/s3/website-s3/lambda.tf
+++ terraform/app/modules/aws/s3/website-s3/lambda.tf
@@ -1,9 +1,3 @@
-data "archive_file" "lambda" {
-  type        = "zip"
-  source_file = "${var.path_to_lambdas}/sns_converter.py"
-  output_path = "${path.module}/lambda_function_payload.zip"
-}
-
 resource "aws_iam_role" "iam_for_lambda" {
   name               = "${var.project_name}-iam-for-lambda"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
@@ -35,7 +29,7 @@ resource "aws_lambda_function" "func" {
   #checkov:skip=CKV_AWS_272: Code-signing is not needed for simple redirect lambda function 
   #ts:skip=AWS.LambdaFunction.Logging.0472 AWS VPC is not needed here for sending notifications
   #ts:skip=AWS.LambdaFunction.Logging.0470 X-Ray is not needed for such lambda and it takes bonus costs
-  filename                       = "${path.module}/lambda_function_payload.zip"
+  filename                       = "${var.path_to_lambdas}/zip/lambda_function_payload.zip"
   function_name                  = "${var.project_name}-lambda-func"
   role                           = aws_iam_role.iam_for_lambda.arn
   handler                        = "sns_converter.lambda_handler"
