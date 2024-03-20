f584e71b7075d6abdf7b482db1e07c35b723c7f8
YapieRT <sokolov.valentyn.work@gmail.com>
2024-03-21 00:46:53 +0200

    fix(#2): add originlatency, change codepipeline execution notification

--- terraform/app/modules/aws/cloudfront/cloudwatch.tf
+++ terraform/app/modules/aws/cloudfront/cloudwatch.tf
@@ -12,8 +12,28 @@ resource "aws_cloudwatch_metric_alarm" "cloudfront-500-errors" {
   provider            = aws.us-east-1
   alarm_name          = "${var.project_name}-AWS-CloudFront-High-5xx-Error-Rate"
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 1
+  evaluation_periods  = 5
   metric_name         = "5xxErrorRate"
+  namespace           = "AWS/CloudFront"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 5
+  treat_missing_data  = "notBreaching"
+  alarm_actions       = [aws_sns_topic.cloudwatch_alarm_notifications.arn]
+  actions_enabled     = true
+
+  dimensions = {
+    DistributionId = aws_cloudfront_distribution.this.id
+    Region         = "Global"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "cloudfront-origin-latency" {
+  provider            = aws.us-east-1
+  alarm_name          = "${var.project_name}-AWS-CloudFront-Origin-Latency"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 5
+  metric_name         = "OriginLatency"
   namespace           = "AWS/CloudFront"
   period              = 60
   statistic           = "Average"
