1804d7f1f71b3d5aa7f058ab340bf5dbd7127f3c
YapieRT <sokolov.valentyn.work@gmail.com>
2024-04-16 15:04:35 +0300

    fix(#2): change terraform version, fix buildspec

--- aws/scripts/terraform_installation.sh
+++ aws/scripts/terraform_installation.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/tfutils/tfenv.git ~/.tfenv
 echo $(export PATH="$HOME/.tfenv/bin:$PATH") >>~/.bash_profile
 export PATH="$HOME/.tfenv/bin:$PATH"
-tfenv install 1.5.5
-tfenv use 1.5.5
+tfenv install 1.4.7
+tfenv use 1.4.7
