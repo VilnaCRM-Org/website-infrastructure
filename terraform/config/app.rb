7db6a5871d77575c7078d7b41dabe24e54836bff
YapieRT <sokolov.valentyn.work@gmail.com>
2024-04-16 13:38:48 +0300

    fix(#2): minor fixes

--- terraform/config/app.rb
+++ terraform/config/app.rb
@@ -1,7 +1,7 @@
 # Docs: https://terraspace.cloud/docs/config/reference/
 Terraspace.configure do |config|
   # config.logger.level = :info
-  config.all.concurrency = 20
+  # config.all.concurrency = 20
   config.build.copy_modules = true
   # To enable Terraspace Cloud set config.cloud.org
   # config.cloud.org = "ORG"          # required: replace with your org. only letters, numbers, underscore and dashes allowed
