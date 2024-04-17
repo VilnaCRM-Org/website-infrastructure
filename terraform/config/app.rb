97949aac1128766f256f6ca87775fd47ec0cc1df
YapieRT <sokolov.valentyn.work@gmail.com>
2024-04-17 15:28:47 +0300

    fix(#2): comment removal, fix to CodePipeline

--- terraform/config/app.rb
+++ terraform/config/app.rb
@@ -1,12 +1,4 @@
-# Docs: https://terraspace.cloud/docs/config/reference/
 Terraspace.configure do |config|
-  # config.logger.level = :info
   config.all.concurrency = 10
   config.build.copy_modules = true
-  # To enable Terraspace Cloud set config.cloud.org
-  # config.cloud.org = "ORG"          # required: replace with your org. only letters, numbers, underscore and dashes allowed
-  # config.cloud.project = "main"     # optional. main is the default project name. only letters, numbers, underscore and dashes allowed
-
-  # Uncomment to enable Cost Estimation. See: http://terraspace.cloud/docs/cloud/cost-estimation/
-  # config.cloud.cost.enabled = true
 end
