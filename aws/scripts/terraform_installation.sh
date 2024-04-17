68158d7b8912919f95ecebaba82c316e686e2807
YapieRT <sokolov.valentyn.work@gmail.com>
2024-04-18 01:52:22 +0300

    fix(#2): rework buildspec

--- aws/scripts/terraform_installation.sh
+++ aws/scripts/terraform_installation.sh
@@ -1,3 +1,9 @@
+echo "## Install OpenTofu"
+curl --proto "=https" --tlsv1.2 -fsSL https://get.opentofu.org/install-opentofu.sh -o install-opentofu.sh
+chmod +x install-opentofu.sh
+./install-opentofu.sh --install-method rpm
+rm install-opentofu.sh
+echo "## Install Terraform"
 git clone https://github.com/tfutils/tfenv.git ~/.tfenv
 echo $(export PATH="$HOME/.tfenv/bin:$PATH") >>~/.bash_profile
 export PATH="$HOME/.tfenv/bin:$PATH"
