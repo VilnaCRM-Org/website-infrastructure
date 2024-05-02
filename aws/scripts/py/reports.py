c954d13e4a43efdb9432b7f7fa544e553edea0d9
YapieRT <sokolov.valentyn.work@gmail.com>
2024-05-02 16:46:48 +0300

    fix(#2): refactor reports.py

--- aws/scripts/py/reports.py
+++ aws/scripts/py/reports.py
@@ -3,103 +3,129 @@ import subprocess
 import glob
 import json
 
-region = os.environ['AWS_REGION']
-account_id = os.environ['ACCOUNT_ID']
-build_id = os.environ['CODEBUILD_BUILD_ID']
-git_repository_last_commit_sha = os.environ['WEBSITE_GIT_REPOSITORY_LAST_COMMIT_SHA']
-git_repository_link = os.environ['WEBSITE_GIT_REPOSITORY_LINK']
-git_author = os.environ['WEBSITE_GIT_REPOSITORY_LAST_COMMIT_AUTHOR']
-git_name = os.environ['WEBSITE_GIT_REPOSITORY_LAST_COMMIT_NAME']
+def get_environment_variables():
+    return {
+        "region": os.environ['AWS_REGION'],
+        "account_id": os.environ['ACCOUNT_ID'],
+        "build_id": os.environ['CODEBUILD_BUILD_ID'],
+        "git_repository_last_commit_sha": os.environ['WEBSITE_GIT_REPOSITORY_LAST_COMMIT_SHA'],
+        "git_repository_link": os.environ['WEBSITE_GIT_REPOSITORY_LINK'],
+        "git_author": os.environ['WEBSITE_GIT_REPOSITORY_LAST_COMMIT_AUTHOR'],
+        "git_name": os.environ['WEBSITE_GIT_REPOSITORY_LAST_COMMIT_NAME'],
+        "lhci_reports_bucket_name": os.environ.get('LHCI_REPORTS_BUCKET'),
+        "test_reports_bucket_name": os.environ.get('TEST_REPORTS_BUCKET')
+    }
 
-if "LHCI_REPORTS_BUCKET" in os.environ:
-    lhci_reports_bucket_name = os.environ['LHCI_REPORTS_BUCKET']
-
-if "TEST_REPORTS_BUCKET" in os.environ:
-    test_reports_bucket_name = os.environ['TEST_REPORTS_BUCKET']
-
-build_name = build_id.split(':')[0]
-
-codebuild_link = f"https://{region}.console.aws.amazon.com/codesuite/codebuild/{account_id}/projects/{build_name}/build/{build_id}?region={region}"
-git_commit_link = f"{git_repository_link}/commit/{git_repository_last_commit_sha}"
-
-reports = []
-
-if "LHCI_DESKTOP_RUN" in os.environ:
-    directory="/root/website/lhci-reports-desktop"
-    lhci_desktop_run_name="Lighthouse Desktop"
-    
+def get_html_files(directory):
     html_files = glob.glob(directory + "/*.html")
-    html_file_names = [file.split("/")[-1] for file in html_files]
-    
-    lhci_desktop_run_links = []
-    for file in html_file_names:
-        temp_link = f"http://{lhci_reports_bucket_name}.s3-website.{region}.amazonaws.com/{build_id}/lhci-reports-desktop/{file}"
-        lhci_desktop_run_links.append(temp_link)
-    
-    reports.append({"name": lhci_desktop_run_name, "links": lhci_desktop_run_links})
-    
-    subprocess.run(['aws', 's3', 'cp', '/root/website/lhci-reports-desktop/.', f's3://{lhci_reports_bucket_name}/{build_id}/lhci-reports-desktop', '--recursive'])
+    return [file.split("/")[-1] for file in html_files]
 
-if "LHCI_MOBILE_RUN" in os.environ:
-    directory = "/root/website/lhci-reports-mobile"
-    lhci_mobile_run_name="Lighthouse Mobile"
-    
-    html_files = glob.glob(directory + "/*.html")
-    html_file_names = [file.split("/")[-1] for file in html_files]
-    
-    lhci_mobile_run_links = []
-    for file in html_file_names:
-        temp_link = f"http://{lhci_reports_bucket_name}.s3-website.{region}.amazonaws.com/{build_id}/lhci-reports-mobile/{file}"
-        lhci_mobile_run_links.append(temp_link)
-    
-    reports.append({"name": lhci_mobile_run_name, "links": lhci_mobile_run_links})
-    
-    subprocess.run(['aws', 's3', 'cp', '/root/website/lhci-reports-mobile/.', f's3://{lhci_reports_bucket_name}/{build_id}/lhci-reports-mobile', '--recursive'])
+def generate_codebuild_link(region, account_id, build_id):
+    build_name = build_id.split(':')[0]
+    return f"https://{region}.console.aws.amazon.com/codesuite/codebuild/{account_id}/projects/{build_name}/build/{build_id}?region={region}"
+
+def generate_git_commit_link(git_repository_link, git_repository_last_commit_sha):
+    return f"{git_repository_link}/commit/{git_repository_last_commit_sha}"
+
+def generate_report(name, links):
+    return {"name": name, "links": links}
+
+def generate_report_link(bucket_name, region, build_id, directory, file='.'):
+    return f"http://{bucket_name}.s3-website.{region}.amazonaws.com/{build_id}/{directory}/{file}"
+
+def copy_to_s3(source_dir, bucket_name, build_id, destination_dir):
+    subprocess.run(['aws', 's3', 'cp', f'{source_dir}/.', f's3://{bucket_name}/{build_id}/{destination_dir}', '--recursive'])
 
 
-if "PW_E2E_RUN" in os.environ:
-    pw_e2e_run_name="Playwright E2E"
-    pw_e2e_run_link=f"http://{test_reports_bucket_name}.s3-website.{region}.amazonaws.com/{build_id}/playwright-e2e-reports"
-    
-    reports.append({"name": pw_e2e_run_name, "links": [pw_e2e_run_link]})
-    
-    subprocess.run(['aws', 's3', 'cp', '/root/website/playwright-e2e-reports/.', f's3://{test_reports_bucket_name}/{build_id}/playwright-e2e-reports', '--recursive'])
+def main():
+    env_variables = get_environment_variables()
 
-if "PW_VISUAL_RUN" in os.environ:
-    pw_visual_run_name = "Playwright Visual"
-    pw_visual_run_link = f"http://{test_reports_bucket_name}.s3-website.{region}.amazonaws.com/{build_id}/playwright-visual-reports"
-    
-    reports.append({"name": pw_visual_run_name, "links": [pw_visual_run_link]})
-    
-    subprocess.run(['aws', 's3', 'cp', '/root/website/playwright-e2e-reports/.', f's3://{test_reports_bucket_name}/{build_id}/playwright-e2e-reports', '--recursive'])
+    region = env_variables["region"]
+    account_id = env_variables["account_id"]
+    build_id = env_variables["build_id"]
+    git_repository_last_commit_sha = env_variables["git_repository_last_commit_sha"]
+    git_repository_link = env_variables["git_repository_link"]
+    lhci_reports_bucket_name = env_variables["lhci_reports_bucket_name"]
+    test_reports_bucket_name = env_variables["test_reports_bucket_name"]
 
-if "MUTATION_RUN" in os.environ:
-    mutation_run_name = "Mutation Test"
-    mutation_run_link = f"http://{test_reports_bucket_name}.s3-website.{region}.amazonaws.com/{build_id}/mutation"
+    codebuild_link = generate_codebuild_link(region, account_id, build_id)
+    git_commit_link = generate_git_commit_link(git_repository_link, git_repository_last_commit_sha)
 
-    reports.append({"name": mutation_run_name, "links": [mutation_run_link]})
-    
-    subprocess.run(['aws', 's3', 'cp', '/root/website/reports/mutation/.', f's3://{test_reports_bucket_name}/{build_id}/mutation', '--recursive'])
+    reports = []
 
-if "UNIT_RUN" in os.environ:
-    unit_run_name = "Unit Test"
-    unit_run_link = f"http://{test_reports_bucket_name}.s3-website.{region}.amazonaws.com/{build_id}/unit"
+    repository_dir = "/root/website"
 
-    reports.append({"name": unit_run_name, "links": [unit_run_link]})
-    
-    subprocess.run(['aws', 's3', 'cp', '/root/website/coverage/lcov-report/.', f's3://{test_reports_bucket_name}/{build_id}/unit', '--recursive'])
+    if "LHCI_DESKTOP_RUN" in os.environ:
+        reports_dir = "lhci-reports-desktop"
 
-data = {
-    "codebuild_link" : codebuild_link,
-    "github" : {
-        "gh_link" : git_commit_link,
-        "sha" : git_repository_last_commit_sha,
-        "author" : git_author,
-        "name" : git_name
-    },
-    "reports" : reports
-}
+        html_files = get_html_files(f"{repository_dir}/{reports_dir}")
 
-json_string = json.dumps(data)
+        links = []
 
-open('payload.json', 'w').write(json_string)
+        for file in html_files:
+            links.append(generate_report_link(lhci_reports_bucket_name, region, build_id, reports_dir, file))
+        
+        reports.append(generate_report("Lighthouse Desktop",links))
+
+        copy_to_s3(f"{repository_dir}/{reports_dir}", lhci_reports_bucket_name, build_id, reports_dir)
+
+    if "LHCI_MOBILE_RUN" in os.environ:
+        reports_dir = "lhci-reports-mobile"
+
+        html_files = get_html_files(f"{repository_dir}/{reports_dir}")
+
+        links = []
+
+        for file in html_files:
+            links.append(generate_report_link(lhci_reports_bucket_name, region, build_id, reports_dir, file))
+        
+        reports.append(generate_report("Lighthouse Mobile",links))
+
+        copy_to_s3(f"{repository_dir}/{reports_dir}", lhci_reports_bucket_name, build_id, reports_dir)
+
+
+    if "PW_E2E_RUN" in os.environ:
+        reports_dir = "playwright-e2e-reports"
+
+        reports.append(generate_report("Playwright E2E",[ generate_report_link(test_reports_bucket_name, region, build_id, reports_dir) ]))
+        
+        copy_to_s3(f"{repository_dir}/{reports_dir}", test_reports_bucket_name, build_id, reports_dir)
+
+    if "PW_VISUAL_RUN" in os.environ:
+        reports_dir = "playwright-visual-reports"
+
+        reports.append(generate_report("Playwright Visual", [ generate_report_link(test_reports_bucket_name, region, build_id, reports_dir) ]))
+        
+        copy_to_s3(f"{repository_dir}/{reports_dir}", test_reports_bucket_name, build_id, reports_dir)
+
+    if "MUTATION_RUN" in os.environ:
+        reports_dir = "reports/mutation"
+
+        reports.append(generate_report("Mutation Test",[ generate_report_link(test_reports_bucket_name, region, build_id, reports_dir) ]))
+        
+        copy_to_s3(f"{repository_dir}/{reports_dir}", test_reports_bucket_name, build_id, reports_dir)
+
+    if "UNIT_RUN" in os.environ:
+        reports_dir = "coverage/lcov-report"
+
+        reports.append(generate_report("Unit Test",[ generate_report_link(test_reports_bucket_name, region, build_id, reports_dir) ]))
+        
+        copy_to_s3(f"{repository_dir}/{reports_dir}", test_reports_bucket_name, build_id, reports_dir)
+
+    data = {
+        "codebuild_link": codebuild_link,
+        "github": {
+            "gh_link": git_commit_link,
+            "sha": git_repository_last_commit_sha,
+            "author": env_variables["git_author"],
+            "name": env_variables["git_name"]
+        },
+        "reports": reports
+    }
+
+    json_string = json.dumps(data)
+
+    open('payload.json', 'w').write(json_string)
+
+if __name__ == "__main__":
+    main()
