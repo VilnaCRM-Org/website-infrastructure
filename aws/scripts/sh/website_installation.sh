928ef46d20f0535238c8ce4b96fde0747583c2f9
YapieRT <sokolov.valentyn.work@gmail.com>
2024-05-06 23:01:29 +0300

    fix(#2): move chown into separate file

--- aws/scripts/sh/website_installation.sh
+++ aws/scripts/sh/website_installation.sh
@@ -6,7 +6,3 @@ echo #### Install pnpm
 npm install -g pnpm
 echo #### Install dependencies
 pnpm i --frozen-lockfile
-chmod +x ~
-chown -R codebuild-user:codebuild-user /codebuild-user/website
-chown -R codebuild-user:codebuild-user $CODEBUILD_SRC_DIR
-chown -R codebuild-user:codebuild-user /usr/local/lib/node_modules/
