3e1c582f2e919b44c40ef8e756fdcc80fbcc7f52
YapieRT <sokolov.valentyn.work@gmail.com>
2024-06-28 02:32:50 +0300

    fix(#2): remove comments

--- terraform/app/stacks/ci-cd-infrastructure/chatbot.tf
+++ terraform/app/stacks/ci-cd-infrastructure/chatbot.tf
@@ -1,64 +1,64 @@
-# module "chatbot_codepipelines" {
-#   count = var.create_slack_notification ? 1 : 0
+module "chatbot_codepipelines" {
+  count = var.create_slack_notification ? 1 : 0
 
-#   source = "../../modules/aws/chatbot"
+  source = "../../modules/aws/chatbot"
 
-#   project_name = "codepipeline"
-#   channel_id   = var.CODEPIPELINE_SLACK_CHANNEL_ID
-#   workspace_id = var.SLACK_WORKSPACE_ID
+  project_name = "codepipeline"
+  channel_id   = var.CODEPIPELINE_SLACK_CHANNEL_ID
+  workspace_id = var.SLACK_WORKSPACE_ID
 
-#   sns_topic_arns = [
-#     module.website_infra_codepipeline.sns_topic_arn,
-#     module.ci_cd_infra_codepipeline.sns_topic_arn,
-#     module.ci_cd_website_codepipeline.codepipeline_sns_topic_arn,
-#     module.sandbox_codepipeline.codepipeline_sns_topic_arn,
-#   ]
+  sns_topic_arns = [
+    module.website_infra_codepipeline.sns_topic_arn,
+    module.ci_cd_infra_codepipeline.sns_topic_arn,
+    module.ci_cd_website_codepipeline.codepipeline_sns_topic_arn,
+    module.sandbox_codepipeline.codepipeline_sns_topic_arn,
+  ]
 
-#   tags = var.tags
+  tags = var.tags
 
-#   depends_on = [
-#     module.website_infra_codepipeline,
-#     module.ci_cd_infra_codepipeline,
-#     module.ci_cd_website_codepipeline
-#   ]
-# }
+  depends_on = [
+    module.website_infra_codepipeline,
+    module.ci_cd_infra_codepipeline,
+    module.ci_cd_website_codepipeline
+  ]
+}
 
-# module "chatbot_reports" {
-#   count = var.create_slack_notification ? 1 : 0
+module "chatbot_reports" {
+  count = var.create_slack_notification ? 1 : 0
 
-#   source = "../../modules/aws/chatbot"
+  source = "../../modules/aws/chatbot"
 
-#   project_name = "reports"
-#   channel_id   = var.REPORT_SLACK_CHANNEL_ID
-#   workspace_id = var.SLACK_WORKSPACE_ID
+  project_name = "reports"
+  channel_id   = var.REPORT_SLACK_CHANNEL_ID
+  workspace_id = var.SLACK_WORKSPACE_ID
 
-#   sns_topic_arns = [
-#     module.ci_cd_website_codepipeline.reports_sns_topic_arn
-#   ]
+  sns_topic_arns = [
+    module.ci_cd_website_codepipeline.reports_sns_topic_arn
+  ]
 
-#   tags = var.tags
+  tags = var.tags
 
-#   depends_on = [
-#     module.ci_cd_website_codepipeline
-#   ]
-# }
+  depends_on = [
+    module.ci_cd_website_codepipeline
+  ]
+}
 
-# module "ci_cd_alerts" {
-#   count = var.create_slack_notification ? 1 : 0
+module "ci_cd_alerts" {
+  count = var.create_slack_notification ? 1 : 0
 
-#   source = "../../modules/aws/chatbot"
+  source = "../../modules/aws/chatbot"
 
-#   project_name = "ci-cd-alerts"
-#   channel_id   = var.CI_CD_ALERTS_SLACK_CHANNEL_ID
-#   workspace_id = var.SLACK_WORKSPACE_ID
+  project_name = "ci-cd-alerts"
+  channel_id   = var.CI_CD_ALERTS_SLACK_CHANNEL_ID
+  workspace_id = var.SLACK_WORKSPACE_ID
 
-#   sns_topic_arns = [
-#     module.cloudwatch_alerts_sns.cloudwatch_alerts_sns_topic_arn
-#   ]
+  sns_topic_arns = [
+    module.cloudwatch_alerts_sns.cloudwatch_alerts_sns_topic_arn
+  ]
 
-#   tags = var.tags
+  tags = var.tags
 
-#   depends_on = [
-#     module.cloudwatch_alerts_sns
-#   ]
-# }
+  depends_on = [
+    module.cloudwatch_alerts_sns
+  ]
+}
