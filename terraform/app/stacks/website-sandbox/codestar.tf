130fc307d8bae608f1810089521c797e2d5c5701
YapieRT <sokolov.valentyn.work@gmail.com>
2024-06-28 01:45:46 +0300

    fix(#3): remove codestar

--- terraform/app/stacks/website-sandbox/codestar.tf
+++
@@ -1,7 +0,0 @@
-module "codestar_connection" {
-  source = "../../modules/aws/codestar"
-
-  github_connection_name = var.github_connection_name
-
-  tags = var.tags
-}
--- terraform/app/stacks/website-sandbox/tfvars/test.tfvars
+++ terraform/app/stacks/website-sandbox/tfvars/test.tfvars
@@ -1,7 +1,5 @@
 project_name = "sandbox-test"
 
-github_connection_name = "Github"
-
 environment = "test"
 
 tags = {
