aa98585070514871131dd412648dcc342d938fb9
Mykyta Mozghovoi <n.mozgowoi2017@gmail.com>
2024-10-18 05:11:53 +0300

    fix(#3): add permission to the role to allow sandbox bucket creation

--- terraform/app/modules/aws/iam/policies/sandbox/data_s3.tf
+++ terraform/app/modules/aws/iam/policies/sandbox/data_s3.tf
@@ -3,6 +3,7 @@ data "aws_iam_policy_document" "s3_policy_doc" {
     sid    = "S3PolicyArtifactBucket"
     effect = "Allow"
     actions = [
+      "s3:CreateBucket",
       "s3:GetObject",
       "s3:PutObject",
       "s3:DeleteObject",
