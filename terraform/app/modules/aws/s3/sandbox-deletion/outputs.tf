6b3b5a3f911a9c4a2b18e15177849f7cf6e5d615
Mykyta Mozghovoi <n.mozgowoi2017@gmail.com>
2024-11-01 01:29:50 +0200

    refactor(#43): adding sensitive in outputs file

--- terraform/app/modules/aws/s3/sandbox-deletion/outputs.tf
+++ terraform/app/modules/aws/s3/sandbox-deletion/outputs.tf
@@ -1,16 +1,19 @@
 output "codepipeline_bucket_arn" {
   description = "ARN of the S3 bucket used for storing CodePipeline artifacts"
   value       = aws_s3_bucket.codepipeline_bucket.arn
+  sensitive   = true
 }
 
 output "codebuild_logs_bucket_arn" {
   description = "ARN of the S3 bucket used for storing CodeBuild logs"
   value       = aws_s3_bucket.codebuild_logs_bucket.arn
+  sensitive   = true
 }
 
 output "access_logs_bucket_arn" {
   description = "ARN of the S3 bucket used for storing access logs"
   value       = aws_s3_bucket.access_logs_bucket.arn
+  sensitive   = true
 }
 
 output "codepipeline_bucket_name" {
