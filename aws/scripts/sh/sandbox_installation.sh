8f340f00ee6e6fd81a596a7eacffdbfed82701dd
Mykyta Mozghovoi <n.mozgowoi2017@gmail.com>
2024-11-12 20:26:40 +0200

    refactor(#62): update make install command

--- aws/scripts/sh/sandbox_installation.sh
+++ aws/scripts/sh/sandbox_installation.sh
@@ -13,4 +13,4 @@ cd /codebuild-user/website/ || exit 1
 echo #### Install pnpm
 npm install -g pnpm || { echo "Error: Failed to install pnpm" >&2; exit 1; }
 echo #### Install dependencies
-make install
+make install || { echo "Error: Failed to install dependencies" >&2; exit 1; }
