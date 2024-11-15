40ad897506745eaae96fc36955820a0e18a96789
Mykyta Mozghovoi <n.mozgowoi2017@gmail.com>
2024-11-15 19:11:38 +0200

    refactor(#81): change bucket name to more meaningful

--- terraform/app/modules/aws/s3/public/outputs.tf
+++ terraform/app/modules/aws/s3/public/outputs.tf
@@ -1,9 +1,9 @@
 output "arn" {
-  value       = aws_s3_bucket.bucket.arn
+  value       = aws_s3_bucket.reports_bucket.arn
   description = "ARN of the public bucket"
 }
 
 output "id" {
-  value       = aws_s3_bucket.bucket.id
+  value       = aws_s3_bucket.reports_bucket.id
   description = "ID of the public bucket"
 }
