9e8d3a365919278cf7e7ff7c66fa8697a4d37190
Mykyta Mozghovoi <n.mozgowoi2017@gmail.com>
2024-11-25 15:39:38 +0200

    feat(#3): add sandbox pipeline infra diagrams

---
+++ diagrams/py/test/sandbox_pipeline_design.py
@@ -0,0 +1,42 @@
+from diagrams import Cluster, Diagram, Edge
+from diagrams.onprem.vcs import Github
+from diagrams.aws.devtools import Codepipeline, Codebuild
+from diagrams.aws.storage import SimpleStorageServiceS3
+from diagrams.aws.management import Chatbot
+from diagrams.aws.integration import SNS
+from diagrams.saas.chat import Slack
+
+with Diagram(
+    "\nSandbox Infrastructure Terraform pipeline Test Design VilnaCRM",
+    show=False,
+    filename="../../img/test/sandbox_pipeline_design",
+):
+    gh = Github("Github Repository")
+    codepipe = Codepipeline("AWS CodePipeline")
+    s3 = SimpleStorageServiceS3("AWS S3 \n Sandbox Bucket")
+    website_infra_codepipeline = Cluster("Sandbox CodePipeline")
+    chatbot = Chatbot("AWS chatbot \n Send notifies \n to Slack")
+    sns = SNS("AWS SNS \n Notify about \n pipeline progress")
+
+    with website_infra_codepipeline:
+        builders = [
+            Codebuild("AWS CodeBuild \n up"),
+            Codebuild("AWS CodeBuild \n deploy"),
+        ]
+
+    gh >> codepipe
+
+    builders[0] >> builders[1]
+
+    codepipe >> builders[0]
+
+    for builder in builders:
+        builder >> s3
+
+    s3 >> Edge()
+
+    builders[1] >> sns
+
+    sns >> chatbot
+
+    chatbot >> Slack("Slack \n Notifications")
