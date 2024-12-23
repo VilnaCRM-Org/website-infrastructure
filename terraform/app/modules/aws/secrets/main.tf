dc12718a988768de30bc9ea54b3e14e41244ddb5
bra1n2h <bra1n2h@users.noreply.github.com>
2024-12-23 16:42:12 +0000

    chore(#5): fix linting issues

--- terraform/app/modules/aws/secrets/main.tf
+++ terraform/app/modules/aws/secrets/main.tf
@@ -8,6 +8,6 @@ resource "random_string" "secret_hash" {
 resource "aws_secretsmanager_secret" "github_token" {
   #checkov:skip=CKV_AWS_149:The KMS encryption of GitHub token is not needed
   #checkov:skip=CKV2_AWS_57:Token rotation is performed by GitHub actions
-  name = "${var.github_token_secret_name}-${random_string.secret_hash.result}"
+  name        = "${var.github_token_secret_name}-${random_string.secret_hash.result}"
   description = "GitHub token for automation"
 }
