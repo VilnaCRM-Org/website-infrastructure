4e769df7f65566a5d257514e3d4580e8340e0560
Mykyta Mozghovoi <n.mozgowoi2017@gmail.com>
2024-12-23 19:36:48 +0200

    refactor(#153): change description

--- terraform/app/modules/aws/secrets/main.tf
+++ terraform/app/modules/aws/secrets/main.tf
@@ -9,5 +9,5 @@ resource "aws_secretsmanager_secret" "github_token" {
   #checkov:skip=CKV_AWS_149:The KMS encryption of GitHub token is not needed
   #checkov:skip=CKV2_AWS_57:Token rotation is performed by GitHub actions
   name        = "${var.github_token_secret_name}-${random_string.secret_hash.result}"
-  description = "GitHub token for automation"
+  description = "GitHub token for sandbox environment deployments and CI/CD automation"
 }
