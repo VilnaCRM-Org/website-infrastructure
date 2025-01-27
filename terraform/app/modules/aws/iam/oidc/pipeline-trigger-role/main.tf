a42303f853204307ea23b44d714dc92b7f040102
Mykyta Mozghovoi <98345491+bra1n2h@users.noreply.github.com>
2025-01-27 21:37:43 +0200

    Fix sandbox frontend (#26)
    * hotfix(#3): fix frontend sandboxes
    
    * fix(#3): remove pip cache
    
    * refactor(#3): remove unnecessary steps
    
    * fix(#3): return checkout step
    
    * chore(#24): fix linting issues
    
    * fix(#3): correct permissions for sandbox bucket
    
    * hotfix(#3): permissions fix
    
    * hotfix(#3): enhance policy
    
    Signed-off-by: Mykyta Mozghovoi <n.mozgowoi2017@gmail.com>

--- terraform/app/modules/aws/iam/oidc/pipeline-trigger-role/main.tf
+++ terraform/app/modules/aws/iam/oidc/pipeline-trigger-role/main.tf
@@ -17,10 +17,7 @@ resource "aws_iam_role" "codepipeline_trigger_role" {
         Condition = {
           StringLike = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-            "token.actions.githubusercontent.com:sub" = [
-              "repo:${var.github_owner}/${var.github_repo}:ref:refs/heads/*",
-              "repo:${var.github_owner}/${var.website_repo}:ref:refs/heads/*"
-            ]
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_owner}/*"
           }
         }
       }
