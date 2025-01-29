1d92ddc336f2ad41bdd9d0ab105eed52a4e6e694
Mykyta Mozghovoi <n.mozgowoi2017@gmail.com>
2025-01-29 18:54:48 +0200

    refactor(#27): remove kms in cloudfront module

--- terraform/app/modules/aws/cloudfront/cloudwatch.tf
+++ terraform/app/modules/aws/cloudfront/cloudwatch.tf
@@ -1,9 +1,8 @@
 resource "aws_cloudwatch_log_group" "waf_web_acl_log_group" {
-  #checkov:skip=CKV_AWS_338: The one year is too much 
+  #checkov:skip=CKV_AWS_338: The one year is too much
+  #checkov:skip=CKV_AWS_158: KMS encryption is not needed
   provider          = aws.us-east-1
   name              = "aws-waf-logs-wafv2-web-acl"
   retention_in_days = 60
-  kms_key_id        = aws_kms_key.cloudwatch_encryption_key.arn
 
-  depends_on = [aws_kms_key_policy.cloudwatch_encryption_key]
 }
