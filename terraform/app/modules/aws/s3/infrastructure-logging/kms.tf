6a059f9b27ba769a799cdd782d64491fc8126d2f
Mykyta Mozghovoi <n.mozgowoi2017@gmail.com>
2025-01-29 18:28:06 +0200

    refactor(#27): delete files with kms
    Signed-off-by: Mykyta Mozghovoi <n.mozgowoi2017@gmail.com>

--- terraform/app/modules/aws/s3/infrastructure-logging/kms.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_kms_key" "s3_kms_key" {
-  #checkov:skip=CKV2_AWS_64:KMS Key Policy is not needed here
-  description             = "This key is used to encrypt bucket objects"
-  deletion_window_in_days = 10
-  enable_key_rotation     = true
-}
