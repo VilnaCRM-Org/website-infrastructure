3334082d13132a5df354417560575b5b2f685bc9
Mykyta Mozhgovoi <n.mozgowoi2017@gmail.com>
2025-03-04 15:39:24 +0200

    refactor(#42): using data http block

--- terraform/app/modules/aws/cloudfront/function.tf
+++ terraform/app/modules/aws/cloudfront/function.tf
@@ -1,8 +1,12 @@
+data "http" "routing_function_source" {
+  url = "https://raw.githubusercontent.com/VilnaCRM-Org/website/133-cloudfront-routing/scripts/cloudfront_routing.js"
+}
+
 resource "aws_cloudfront_function" "routing_function" {
   name    = "routing-function"
   runtime = "cloudfront-js-1.0"
   comment = "Routing logic for CloudFront"
-  publish = true
-
-  code = "console.log('CloudFront Function initialized');"
+  publish = true  
+  
+  code = data.http.routing_function_source.response_body
 }
