8cf2bb91675e72ec944cf16213b33603e7cf6ceb
bra1n2h <98345491+bra1n2h@users.noreply.github.com>
2025-03-04 13:41:30 +0000

    chore(#43): fix linting issues

--- terraform/app/modules/aws/cloudfront/function.tf
+++ terraform/app/modules/aws/cloudfront/function.tf
@@ -6,7 +6,7 @@ resource "aws_cloudfront_function" "routing_function" {
   name    = "routing-function"
   runtime = "cloudfront-js-1.0"
   comment = "Routing logic for CloudFront"
-  publish = true  
-  
+  publish = true
+
   code = data.http.routing_function_source.response_body
 }
