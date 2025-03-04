cc230ecb637ea60caeb99b161990bf02e5dd7c48
Mykyta Mozhgovoi <n.mozgowoi2017@gmail.com>
2025-03-04 14:11:02 +0200

    hotfix(#3): fix frontend sandbox policies

--- terraform/app/modules/aws/iam/roles/github-token-rotation-role/main.tf
+++ terraform/app/modules/aws/iam/roles/github-token-rotation-role/main.tf
@@ -17,7 +17,8 @@ resource "aws_iam_role" "github_actions_role" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
             "token.actions.githubusercontent.com:sub" = [
               "repo:${var.source_repo_owner}/${var.source_repo_name}:ref:refs/heads/main",
-              "repo:${var.source_repo_owner}/${var.website_repo_name}:ref:refs/heads/*"
+              "repo:${var.source_repo_owner}/${var.website_repo_name}:ref:refs/heads/*",
+              "repo:${var.source_repo_owner}/${var.source_repo_name}:pull_request"
             ]
           }
         }
