c8047e87edae2c25b4e3b23d6d9040a1f9ff5666
Mykyta Mozhgovoi <n.mozgowoi2017@gmail.com>
2025-03-18 15:09:30 +0200

    fix(#50): return correct role

--- terraform/app/modules/aws/codepipeline/sandbox/lambda.tf
+++ terraform/app/modules/aws/codepipeline/sandbox/lambda.tf
@@ -8,7 +8,7 @@ resource "aws_lambda_function" "s3_cleanup_lambda" {
   #ts:skip=AWS.LambdaFunction.Logging.0472 AWS VPC is not needed here for sending notifications
   #ts:skip=AWS.LambdaFunction.Logging.0470 X-Ray is not needed for such lambda and it takes bonus costs
   function_name = "s3-cleanup-lambda"
-  role          = var.codepipeline_role_arn
+  role          = aws_iam_role.lambda_execution_role.arn
   runtime       = "python3.9"
   handler       = "s3_cleanup.lambda_handler"
 
