a58d3fb8a5c752cf13826ca5938f37614a07697e
bra1n2h <98345491+bra1n2h@users.noreply.github.com>
2025-03-19 20:09:41 +0000

    chore(#51): fix linting issues

--- aws/lambda/python/s3_cleanup.py
+++ aws/lambda/python/s3_cleanup.py
@@ -1,6 +1,7 @@
 import boto3
 import sys
 
+
 def delete_s3_bucket(bucket_name):
     s3 = boto3.client("s3")
     try:
@@ -16,15 +17,17 @@ def delete_s3_bucket(bucket_name):
     except Exception as e:
         print(f"Error deleting bucket {bucket_name}: {str(e)}")
 
+
 def lambda_handler(event, context):
     bucket_name = event.get("detail", {}).get("bucket_name")
-    
+
     if not bucket_name:
         print("Error: No bucket name provided in the event")
         return
 
     print(f"Received bucket name: {bucket_name}")
     delete_s3_bucket(bucket_name)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
