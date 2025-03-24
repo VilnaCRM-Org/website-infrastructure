725d2c1e9285f8cd29c767eabb09a1186d9cf71a
Mykyta Mozhgovoi <n.mozgowoi2017@gmail.com>
2025-03-24 18:58:27 +0200

    fix(#50): edit name

--- terraform/app/modules/aws/codepipeline/sandbox/lambda.tf
+++ terraform/app/modules/aws/codepipeline/sandbox/lambda.tf
@@ -11,13 +11,14 @@ resource "aws_lambda_function" "sandbox_cleanup_lambda" {
   runtime                        = var.lambda_python_version
   handler                        = "sandbox_cleanup.lambda_handler"
   timeout                        = 300
+  memory_size                    = 256
   reserved_concurrent_executions = var.lambda_reserved_concurrent_executions
 
   filename = "${var.path_to_lambdas}/zip/sandbox_cleanup_function_payload.zip"
 
   depends_on = [
-    aws_iam_role.lambda_cleanup_function_role,
-    aws_iam_policy.s3_cleanup_function_policy
+    aws_iam_role.sandbox_cleanup_function_role,
+    aws_iam_policy.sandbox_cleanup_function_policy
   ]
 }
 
