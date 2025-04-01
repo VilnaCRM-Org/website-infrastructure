c132e5a1397104f6702be41bffb9472f5e5286ca
Mykyta Mozhgovoi <n.mozgowoi2017@gmail.com>
2025-04-01 16:45:21 +0300

    fix(#52): change branch length to 47

--- aws/scripts/sh/sanitize_branch.sh
+++ aws/scripts/sh/sanitize_branch.sh
@@ -4,9 +4,9 @@ set -e
 # Convert BRANCH_NAME to lowercase and remove invalid characters
 sanitized_branch=$(echo "$BRANCH_NAME" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9.-]//g' | sed -E 's/^[.-]+|[.-]+$//g' | tr '.' '-')
 
-# If the sanitized branch name is longer than 60 characters, truncate it
-if [[ ${#sanitized_branch} -gt 60 ]]; then
-  sanitized_branch="${sanitized_branch:0:60}"
+# If the sanitized branch name is longer than 47 characters, truncate it
+if [[ ${#sanitized_branch} -gt 47 ]]; then
+  sanitized_branch="${sanitized_branch:0:47}"
 fi
 
 # Export the cleaned branch name so it's available in subsequent scripts
