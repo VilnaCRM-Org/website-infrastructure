ec606ba30b90892969f6999099b5eb89f90a7c81
Mykyta Mozhgovoi <n.mozgowoi2017@gmail.com>
2025-04-01 23:40:53 +0300

    fix(#52): add cut to rule name

--- aws/scripts/sh/create_eventbridge_rule.sh
+++ aws/scripts/sh/create_eventbridge_rule.sh
@@ -12,7 +12,7 @@ if [ -z "$BRANCH_NAME" ]; then
 fi
 
 bucket_name="${PROJECT_NAME}-${BRANCH_NAME}"
-rule_name="sandbox-cleanup-$(echo "$bucket_name" | sed 's/\./-/g')"
+rule_name="sandbox-cleanup-$(echo "$bucket_name" | sed 's/\./-/g' | cut -c1-60)"
 region=${AWS_REGION:-$(aws configure get region)}
 
 start_time=$(date -u -d "+7 days" +"%M %H %d %m %Y")
