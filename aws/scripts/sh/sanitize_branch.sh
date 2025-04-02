6a297ffbc254b2cf83665e55f295ea890dfd61c4
Mykyta Mozhgovoi <n.mozgowoi2017@gmail.com>
2025-04-02 17:41:33 +0300

    fix(#52): fix bash linting issue

--- aws/scripts/sh/sanitize_branch.sh
+++ aws/scripts/sh/sanitize_branch.sh
@@ -5,7 +5,7 @@ set -e
 sanitized_branch=$(echo "$BRANCH_NAME" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9.-]//g' | sed -E 's/^[.-]+|[.-]+$//g')
 
 # If the sanitized branch name is longer than 47 characters, truncate it
-if [ "$(echo "$sanitized_branch" | wc -c)" -gt 47 ]; then
+if [ "$(echo "$sanitized_branch" | LC_ALL=C wc -c)" -gt 47 ]; then
   sanitized_branch=$(echo "$sanitized_branch" | cut -c 1-47)
 fi
 
