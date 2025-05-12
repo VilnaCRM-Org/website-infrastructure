8c42a66875b2da0e8b0063a1b30cc9ac8240fbd2
Mykyta Mozhgovoi <n.mozgowoi2017@gmail.com>
2025-05-12 20:09:09 +0300

    fix(#60): add dir creation

--- aws/scripts/sh/sandbox_installation.sh
+++ aws/scripts/sh/sandbox_installation.sh
@@ -5,7 +5,7 @@ n install "${NODEJS_VERSION:?Node.js version is not set}" || { echo "Error: Fail
 # Validate required environment variables
 : "${WEBSITE_GIT_REPOSITORY_LINK:?Repository link is not set}"
 : "${BRANCH_NAME:?Branch name is not set}"
-
+mkdir -p /codebuild-user/website
 git clone -b "$BRANCH_NAME" "$WEBSITE_GIT_REPOSITORY_LINK.git" /codebuild-user/website || {
     echo "Error: Failed to clone repository" >&2
     exit 1
