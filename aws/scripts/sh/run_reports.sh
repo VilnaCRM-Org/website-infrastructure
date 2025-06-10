b2cb6917a35e33a8b854b5f8fe9a83dfdc51a72b
Mykyta Mozhgovoi <n.mozgowoi2017@gmail.com>
2025-06-11 01:52:43 +0300

    fix(#60): fix reports script execution

--- aws/scripts/sh/run_reports.sh
+++ aws/scripts/sh/run_reports.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
+apk add --no-cache python3 aws-cli
 
-# Выполнить python-скрипт
 python3 "${CODEBUILD_SRC_DIR}/${SCRIPT_DIR}/py/reports.py"
 
-# Вызвать AWS Lambda с payload из файла
 aws lambda invoke --function-name "ci-cd-website-$ENVIRONMENT-reports-notification" --cli-binary-format raw-in-base64-out --payload file://payload.json output.txt
