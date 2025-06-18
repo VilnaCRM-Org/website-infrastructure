8275f85149f49c90282ed8aa004aaa78a69cf71f
Mykyta Mozhgovoi <n.mozgowoi2017@gmail.com>
2025-06-19 01:58:43 +0300

    refactor(#60): add error handling

--- aws/scripts/sh/setup_docker.sh
+++ aws/scripts/sh/setup_docker.sh
@@ -11,6 +11,9 @@ echo "#### Starting Docker daemon..."
 nohup /usr/local/bin/dockerd --host=unix:///var/run/docker.sock --host=tcp://127.0.0.1:2375 --storage-driver=overlay2 &
 
 # Wait for Docker to be ready
-timeout 15 sh -c 'until docker info; do echo "Waiting for Docker to start..."; sleep 1; done'
-
-echo "✅ Docker daemon started successfully" 
+if timeout 15 sh -c 'until docker info; do echo "Waiting for Docker to start..."; sleep 1; done'; then
+    echo "✅ Docker daemon started successfully"
+else
+    echo "❌ Docker daemon failed to start within 15 seconds"
+    exit 1
+fi 
