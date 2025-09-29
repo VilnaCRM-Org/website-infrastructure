c25c79020a08a250499113e0d7307efc322aba3a
bra1n2h <n.mozgowoi2017@gmail.com>
2025-09-29 19:17:40 +0300

    chore(#83): update function url

--- terraform/app/modules/aws/cloudfront/variables.tf
+++ terraform/app/modules/aws/cloudfront/variables.tf
@@ -51,7 +51,7 @@ variable "cloudfront_configuration" {
 variable "cloudfront_routing_function_url" {
   type        = string
   description = "CloudFront Routing Function URL"
-  default     = "https://raw.githubusercontent.com/VilnaCRM-Org/website/main/scripts/cloudfront_routing.js"
+  default     = "https://raw.githubusercontent.com/VilnaCRM-Org/website/229-fix-cloudfront-500-error-on-non-existent-paths/scripts/cloudfront_routing.js"
 }
 
 variable "cloudfront_custom_error_responses" {
