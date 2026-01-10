f4b9f0d45fcfcd60ca0eade5447f994580113a1d
Dmytro Romanenko <dmytroromanenko901@proton.me>
2026-01-10 16:08:33 +0200

    Revert "fix: pin CloudFront TLS minimum"
    This reverts commit 0de81317eae7f545ce0c0b7a80046602e8776475.

--- terraform/app/modules/aws/cloudfront/locals.tf
+++ terraform/app/modules/aws/cloudfront/locals.tf
@@ -9,4 +9,10 @@ locals {
     bad_inputs_rule           = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
     rate_limit_rule           = "AWS-RateLimitRuleSet"
   }
+
+  cloudfront_minimum_protocol_version = lookup(
+    var.cloudfront_configuration,
+    "minimum_protocol_version",
+    "TLSv1.2_2021"
+  )
 }
